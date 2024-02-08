135ccbb279302501777a70fb09124a778f9644c2
sultonqulnd <sultonqulnd@gmail.com>
2024-02-08 20:28:06 +0500

    added hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem voluptates
+        incidunt quae, consequatur unde quod optio excepturi deserunt. Magnam,
+        ab inventore. Eos mollitia, ratione omnis tempore molestiae natus
+        nostrum suscipit repellendus expedita deleniti enim explicabo quae iusto
+        sequi. Vel laudantium, eveniet vero eaque reprehenderit voluptatem
+        dicta, sint autem veritatis, doloremque dolorem quae? Reprehenderit
+        similique dignissimos tempore odit rerum quasi ipsa non, numquam
+        quisquam eum, dolorem nam fugit ea temporibus iure delectus sequi!
+        Incidunt magni perspiciatis suscipit, earum similique voluptatum omnis
+        eligendi doloribus sequi ex. Dicta velit illo et ipsam consequuntur
+        quisquam voluptate fugit vero quasi. Dignissimos sunt quidem iure natus.
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
